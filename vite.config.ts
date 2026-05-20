9ce8646d552f9892afbcb62a48c66d6f2c4e4b5b
Vitalii Khustochka <xyctka@gmail.com>
2026-05-19 20:32:09 -0500

    Set correct base.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/puzzle-2026/',
   build: {
     rollupOptions: {
       input: {
