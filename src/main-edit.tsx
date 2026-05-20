e9dc2313c68a6b83622be55289a7577717485cba
Vitalii Khustochka <xyctka@gmail.com>
2026-05-19 20:29:02 -0500

    Build edit.html by default.

--- src/main-edit.tsx
+++ src/main-edit.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import EditApp from './EditApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <EditApp />
   </StrictMode>,
 )
