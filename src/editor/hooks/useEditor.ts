ac4f03d9dab6e39c5802d60b680777581cfd3c12
Vitalii Khustochka <xyctka@gmail.com>
2026-05-23 13:06:19 -0500

    Correct error message.

--- src/editor/hooks/useEditor.ts
+++ src/editor/hooks/useEditor.ts
@@ -3,6 +3,6 @@ import { EditorContext } from '../contexts/EditorContext'
 
 export function useEditor() {
   const ctx = useContext(EditorContext);
-  if (!ctx) throw new Error('useCategories must be used within CategoriesProvider');
+  if (!ctx) throw new Error('useEditor must be used within EditorProvider');
   return ctx;
 }
