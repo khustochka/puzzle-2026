b757304ae4bcae1ea147f2a4f8d2d67b9d62ed09
Vitalii Khustochka <xyctka@gmail.com>
2026-05-23 00:12:40 -0500

    Random ids for demo categories.

--- src/EditApp.tsx
+++ src/EditApp.tsx
@@ -5,29 +5,29 @@ import { ErrorBoundary } from './components/ErrorBoundary'
 
 const initialCategories: EditorCategory[] = [
   {
-    "id": "6c409d90-e9f0-4784-9933-168de39dcc77",
+    "id": crypto.randomUUID(),
     "name": "Famous locomotives",
     "entries": [
       {
-        "id": "f0302e3c-eee5-4f5a-a1d4-d7a4c3b6965d",
+        "id": crypto.randomUUID(),
         "value": "Salamanca"
       },
       {
-        "id": "cc0caa61-04fd-46ec-bb1c-9f4b2b5e7d5b",
+        "id": crypto.randomUUID(),
         "value": "CN-2747"
       }
     ]
   },
   {
-    "id": "fc8a2208-7b4a-4785-871a-f4980e452058",
+    "id": crypto.randomUUID(),
     "name": "Famous ships",
     "entries": [
       {
-        "id": "85160c28-1981-4fa5-a02a-a88393e553b8",
+        "id": crypto.randomUUID(),
         "value": "Santa Maria"
       },
       {
-        "id": "8b318e8e-1f3a-4abc-9a6b-fca7e35d744d",
+        "id": crypto.randomUUID(),
         "value": "Titanic"
       }
     ]
